b3fa29952e217bc95d38ac09b75462ca22366541
SebekGM <s.gasior97@gmail.com>
2026-09-05 05:55:34 +0200

    Zmiana kroju czcionki

--- app/layout.js
+++ app/layout.js
@@ -10,9 +10,8 @@ export default function RootLayout({ children }) {
     <html lang="pl">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap" rel="stylesheet" />
       </head>
       <body>{children}</body>
     </html>
